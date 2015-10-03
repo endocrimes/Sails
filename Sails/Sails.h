07e9f4bd8345c7b34f0110fc2ea36850069876eb
Daniel Tomlinson <Dan@Tomlinson.io>
2015-10-03 13:52:03 +0100

    Initial Commit

---
+++ Sails/Sails.h
@@ -0,0 +1,19 @@
+//
+//  Sails.h
+//  Sails
+//
+//  Created by Daniel Tomlinson on 03/10/2015.
+//  Copyright © 2015 Daniel Tomlinson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Sails.
+FOUNDATION_EXPORT double SailsVersionNumber;
+
+//! Project version string for Sails.
+FOUNDATION_EXPORT const unsigned char SailsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sails/PublicHeader.h>
+
+
